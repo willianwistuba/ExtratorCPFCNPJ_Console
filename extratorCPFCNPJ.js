1e45195319ae82b004a7a75b63ae5f260e3a5f10
willian.wistuba <52437353+willianwistuba@users.noreply.github.com>
2023-10-18 05:23:32 -0300

    Update extratorCPFCNPJ.js
    corrige bugs referentes aos resultados de CPF

--- extratorCPFCNPJ.js
+++ extratorCPFCNPJ.js
@@ -6,29 +6,40 @@ function extractCPFCNPJAndGenerateLinks() {
   const cpfCnpjRegex = /\d{3}\.\d{3}\.\d{3}-\d{2}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}/g;
 
   // Extrai os números de CPF e CNPJ
-  const cpfCnpjSet = new Set();
+  const cpfArray = [];
+  const cnpjArray = [];
   const pageHtml = document.documentElement.outerHTML;
   let match = cpfCnpjRegex.exec(pageHtml);
   while (match) {
     // Remove a formatação dos números de CPF e CNPJ
     const unformattedCpfCnpj = match[0].replace(/\D/g, '');
 
-    // Adiciona os números de CPF e CNPJ desformatados ao conjunto para eliminar duplicatas
-    cpfCnpjSet.add(unformattedCpfCnpj);
+    if (unformattedCpfCnpj.length === 11) { // É um CPF
+      cpfArray.push(unformattedCpfCnpj);
+    } else if (unformattedCpfCnpj.length === 14) { // É um CNPJ
+      cnpjArray.push(unformattedCpfCnpj);
+    }
+    
     match = cpfCnpjRegex.exec(pageHtml);
   }
 
-  // Gera links para os números de CNPJ desformatados a partir do conjunto
-  const links = Array.from(cpfCnpjSet).map((cnpj) => {
+  // Gera links apenas para os números de CNPJ
+  const cnpjLinks = cnpjArray.map((cnpj) => {
     const receitaWSLink = `https://receitaws.com.br/v1/cnpj/${cnpj}`;
     const minhaReceitaLink = `https://minhareceita.org/${cnpj}`;
     return `<a href="${receitaWSLink}" target="_blank">${receitaWSLink}</a><br><a href="${minhaReceitaLink}" target="_blank">${minhaReceitaLink}</a><br>`;
   });
 
-  // Abre uma nova janela com os links
-  const linksString = links.join('\n');
+  // Abre uma nova janela com os links para CNPJ e exibe os números de CPF
+  const cnpjLinksString = cnpjLinks.join('\n');
   const newWindow = window.open('', '_blank');
-  newWindow.document.write(linksString);
+  newWindow.document.write(cnpjLinksString);
+  newWindow.document.write('<h1>Números de CPF:</h1>');
+  newWindow.document.write('<ul>');
+  for (const cpf of cpfArray) {
+    newWindow.document.write(`<li>${cpf}</li>`);
+  }
+  newWindow.document.write('</ul>');
   newWindow.document.close();
 }
 
